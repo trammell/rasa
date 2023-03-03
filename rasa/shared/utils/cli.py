419fd9ab88868e76d8fc76140efc52a7bbb49659
John Trammell <22057+trammell@users.noreply.github.com>
2023-03-03 10:36:44 -0600

    Fix docstrings to satisfy linter

--- rasa/shared/utils/cli.py
+++ rasa/shared/utils/cli.py
@@ -20,8 +20,8 @@ def print_color(*args: Any, color: Text) -> None:
     """Print the given arguments to STDOUT in the specified color.
 
     Args:
-        args (:obj:`list` of :obj:`Any`): A list of objects to be printed.
-        color (:obj:`Text`): A textual representation of the color.
+        args: A list of objects to be printed.
+        color: A textual representation of the color.
     """
     output = rasa.shared.utils.io.wrap_with_color(*args, color=color)
     try:
@@ -42,17 +42,16 @@ def print_success(*args: Any) -> None:
     """Print the given arguments to STDOUT in green, indicating success.
 
     Args:
-        args (:obj:`list` of :obj:`Any`): A list of objects to be printed.
+        args: A list of objects to be printed.
     """
     print_color(*args, color=rasa.shared.utils.io.bcolors.OKGREEN)
 
 
 def print_info(*args: Any) -> None:
-    """Print the given arguments to STDOUT in blue, indicating an informational
-    message.
+    """Print the given arguments to STDOUT in blue.
 
     Args:
-        args (:obj:`list` of :obj:`Any`): A list of objects to be printed.
+        args: A list of objects to be printed.
     """
     print_color(*args, color=rasa.shared.utils.io.bcolors.OKBLUE)
 
@@ -61,7 +60,7 @@ def print_warning(*args: Any) -> None:
     """Print the given arguments to STDOUT in a color indicating a warning.
 
     Args:
-        args (:obj:`list` of :obj:`Any`): A list of objects to be printed.
+        args: A list of objects to be printed.
     """
     print_color(*args, color=rasa.shared.utils.io.bcolors.WARNING)
 
@@ -70,7 +69,7 @@ def print_error(*args: Any) -> None:
     """Print the given arguments to STDOUT in a color indicating an error.
 
     Args:
-        args (:obj:`list` of :obj:`Any`): A list of objects to be printed.
+        args: A list of objects to be printed.
     """
     print_color(*args, color=rasa.shared.utils.io.bcolors.FAIL)
 
@@ -79,8 +78,8 @@ def print_error_and_exit(message: Text, exit_code: int = 1) -> NoReturn:
     """Print an error message and exit the application.
 
     Args:
-        message (:obj:`Text`): The error message to be printed.
-        exit_code (int): The program exit code, defaults to 1.
+        message: The error message to be printed.
+        exit_code: The program exit code, defaults to 1.
     """
     print_error(message)
     sys.exit(exit_code)
